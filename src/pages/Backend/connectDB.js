23e59ecb505e77b6ffde5cc28cd918e4b0ca0941
Zarakkhan-dev <119649492+Zarakkhan-dev@users.noreply.github.com>
2023-12-02 22:51:17 +0500

    dwqd

--- src/pages/Backend/connectDB.js
+++ src/pages/Backend/connectDB.js
@@ -2,7 +2,7 @@
 import mongoose from 'mongoose';
 const connectDB = async () => {
   try {
-    await mongoose.connect(process.env.MongoUrl,
+    await mongoose.connect(`${process.env.MongoUrl}`,
       {URLSearchParams:true}
       );
     console.log('MongoDB connected successfully');
