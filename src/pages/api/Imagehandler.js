dff0cb6856af3ec5f5e868820045336ae4ef7134
Zarakkhan-dev <119649492+Zarakkhan-dev@users.noreply.github.com>
2023-12-02 23:18:53 +0500

    asfa

--- src/pages/api/Imagehandler.js
+++ src/pages/api/Imagehandler.js
@@ -1,11 +1,11 @@
 
 import connectDB from "../Backend/connectDB";
 import user from "../Backend/User_Schema";
+connectDB();
 export default async function handler(req, res) {
     if(req.method==='POST'){
   const {imageaddress} = req.body
      try {
-       connectDB();
             const saved =new user ({imageaddress})
             await saved.save();
             res.status(201).json({message:"images Saved"});
@@ -15,7 +15,6 @@ export default async function handler(req, res) {
     }
     if(req.method==='GET'){
         try {
-            connectDB();
         const response = await user.find({});
         res.status(202).json({response});
           } catch (error) {
