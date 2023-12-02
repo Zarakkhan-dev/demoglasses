e7cb5b286411e031b95ae98f3818e1f8eb476666
Zarakkhan-dev <119649492+Zarakkhan-dev@users.noreply.github.com>
2023-12-02 22:07:57 +0500

    new change

--- src/Component/ImageUpload/ImageUpload.jsx
+++ src/Component/ImageUpload/ImageUpload.jsx
@@ -51,7 +51,7 @@ const ImageUpload = () => {
     Getresponse();
 
     console.log(UserData);
-  }, [file]);
+  }, [imageaddress]);
 
   return (
     <div className="mt-10 ">
@@ -86,8 +86,8 @@ const ImageUpload = () => {
           <span className="relative">Upload The Frame</span>
         </button>
       </form>
-      <Link href="/displayglasses">
-        <button className="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden font-bold rounded-full shadow-2xl group text-white bg-blue-400">
+      <Link href="/displayglasses" className="flex justify-center my-2">
+        <button className="relative inline-flex items-center justify-center px-4 py-2 overflow-hidden font-bold rounded-full shadow-2xl group text-white bg-blue-400 ">
           <span className="absolute inset-0 w-full h-full transition duration-300 ease-out opacity-0 bg-gradient-to-br from-pink-600 via-purple-700 to-blue-400 group-hover:opacity-100"></span>
           <span className="absolute top-0 left-0 w-full bg-gradient-to-b from-white to-transparent opacity-5 h-1/3"></span>
           <span className="absolute bottom-0 left-0 w-full h-1/3 bg-gradient-to-t from-white to-transparent opacity-5"></span>
@@ -95,7 +95,7 @@ const ImageUpload = () => {
           <span className="absolute bottom-0 right-0 w-4 h-full bg-gradient-to-l from-white to-transparent opacity-5"></span>
           <span className="absolute inset-0 w-full h-full border border-white rounded-md opacity-10"></span>
           <span className="absolute w-0 h-0 transition-all duration-300 ease-out bg-white rounded-full group-hover:w-56 group-hover:h-56 opacity-5"></span>
-          <span className="relative">Upload The Frame</span>
+          <span className="relative">Demo of Frames</span>
         </button>
       </Link>
       <div className="image flex justify-center mt-12">
--- src/Component/SliderGlasses/SliderGlasses.jsx
+++ src/Component/SliderGlasses/SliderGlasses.jsx
@@ -4,7 +4,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import axios from 'axios';
 import { Pagination } from 'swiper/modules';
-
+import Image from 'next/image';
 export default function App() {
   const [UserData, updateData] = useState([]);
   useEffect(() => {
